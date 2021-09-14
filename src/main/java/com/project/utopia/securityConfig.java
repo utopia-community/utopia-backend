178e14c617a7526bf1fcbb5bda30bb9eed76fb3a
simon zhang <zhang86036055@gmail.com>
2021-09-13 22:00:01 -0700

    Security 1st try

---
+++ src/main/java/com/project/utopia/securityConfig.java
@@ -0,0 +1,44 @@
+package com.project.utopia;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
+
+public class securityConfig extends WebSecurityConfigurerAdapter {
+    // this class is for Spring to filter info based on logged in user's authority
+    //right now copied from DoorDash
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin()
+                .failureForwardUrl("/login?error=true");
+        http
+                .authorizeRequests()
+                .antMatchers("/request").hasAuthority("ROLE_USER")
+                .anyRequest().permitAll();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT email, password, enabled FROM users WHERE email=?")
+                .authoritiesByUsernameQuery("SELECT email, authorities FROM authorities WHERE email=?");
+
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
--- src/main/java/com/project/utopia/service/UserService.java
+++ src/main/java/com/project/utopia/service/UserService.java
@@ -1,4 +1,21 @@
 package com.project.utopia.service;
 
+import com.project.utopia.dao.UserDao;
+import com.project.utopia.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserService {
+    // service 层的作用: 把传进来的customer 绑定一个新的cart 然后调用dao写入db
+    @Autowired
+    private UserDao userDao;
+
+    public void signUp(User user, String authority) {
+
+        user.setEnabled(true);
+        userDao.signUp(user, authority);
+    }
+
+    public User getCustomer(String email) {
+        return userDao.getUser(email);
+    }
 }
