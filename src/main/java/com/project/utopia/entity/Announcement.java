362816108084166ea818a8eff226d42b7312f0c0
simon zhang <zhang86036055@gmail.com>
2021-09-14 18:12:46 -0700

    auth updates

--- src/main/java/com/project/utopia/entity/Announcement.java
+++ src/main/java/com/project/utopia/entity/Announcement.java
@@ -1,4 +1,20 @@
 package com.project.utopia.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "announcements")
 public class Announcement {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
