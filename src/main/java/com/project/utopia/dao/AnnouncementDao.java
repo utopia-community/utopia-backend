0cf3231a900c584bf64c3e307bf9eba852330967
yuhuiyingri <xuehm05@gmail.com>
2021-09-15 20:57:54 -0400

    Initialized class AnnouncementDao.

--- src/main/java/com/project/utopia/dao/AnnouncementDao.java
+++ src/main/java/com/project/utopia/dao/AnnouncementDao.java
@@ -1,7 +1,18 @@
 package com.project.utopia.dao;
 
+import com.project.utopia.entity.Announcement;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class AnnouncementDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public List<Announcement> getAllAnnouncements() {
+        return null;
+    }
 }
