c91ccd4b244a6989e5c547feaed8e58a6c6b1e27
simon zhang <zhang86036055@gmail.com>
2021-09-16 23:17:54 -0700

    updated request

--- src/main/java/com/project/utopia/entity/Request.java
+++ src/main/java/com/project/utopia/entity/Request.java
@@ -3,10 +3,18 @@ package com.project.utopia.entity;
 import java.io.Serializable;
 import javax.persistence.*;
 
-//@Entity
-//@Table(name = "requests")
+@Entity
+@Table(name = "requests")
 public class Request implements Serializable {
     private static final long serialVersionUID = 105L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int requestId;
 
+    private String title;
+    private String content;
+
+    @ManyToOne
+    private Customer customer;
 }
