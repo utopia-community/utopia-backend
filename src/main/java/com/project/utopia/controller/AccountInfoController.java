c9f4ba1e33fb917a8ea902fab09173a592720fcb
simon zhang <zhang86036055@gmail.com>
2021-09-17 00:03:16 -0700

    logout and account info
    Added Security Config logout function
    Added Account info function

---
+++ src/main/java/com/project/utopia/controller/AccountInfoController.java
@@ -0,0 +1,29 @@
+package com.project.utopia.controller;
+
+import com.project.utopia.entity.Customer;
+import com.project.utopia.holder.response.AccountInfoResponseBody;
+import com.project.utopia.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountInfoController {
+    @Autowired
+    private CustomerService customerService;
+
+    @RequestMapping(value = "/accountinfo", method = RequestMethod.GET)
+    public ResponseEntity<Object> accountInfo() {
+        System.out.println("Enter the accountInfo controller");
+        Customer customer = customerService.getCurrentCustomer();
+        AccountInfoResponseBody response = new AccountInfoResponseBody();
+        response.setEmail(customer.getUser().getEmailId());
+        response.setFirstName(customer.getFirstName());
+        response.setLastName(customer.getLastName());
+        response.setBillingAddress(customer.getAddress());
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
