1b6100d944472773d15ab7602b0db78ae6482d44
simon zhang <zhang86036055@gmail.com>
2021-09-19 15:48:15 -0700

    implemented access control
    成功实现了 request有的url 只有customer可以用
    另外的一些url（如 all requests）只有admin 可以用

--- src/main/java/com/project/utopia/SecurityConfig.java
+++ src/main/java/com/project/utopia/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests() // 指明权限，*是任意字符；**是可以匹配/a/b这种多个level的。
                 .antMatchers("/announcements/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
-                .antMatchers("/requests/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                .antMatchers("/setRequestStatus/**").hasAnyAuthority( "ROLE_ADMIN")
+                .antMatchers("/allRequests/**").hasAnyAuthority( "ROLE_ADMIN")
                 .antMatchers("/new-announcement/**").hasAnyAuthority( "ROLE_ADMIN")
                 .antMatchers("/profile/**").hasAnyAuthority("ROLE_USER")
                 .anyRequest().permitAll();
